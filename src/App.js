46f7f78fd9f26fc0c4023a32b45e705ca50a6463
rifat-islam <alaminict@outlook.com>
2024-02-10 19:35:53 +0600

    https://github.com/alaminlife/ultra-nation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Country from './Componints/Country/Country';
 function App() {
 
   const [coutries,setCoutries] = useState ([]);
+  const [Cart,setCart] = useState ([]);
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
@@ -13,15 +14,20 @@ function App() {
       setCoutries(data))
     .catch(error => console.error(error))
   }, [])
+  const handleAddCountry = (Country) => {
+  const newCart = [...Cart,Country];
+  setCart(newCart);
+  }
 
 
 
   return (
     <div className="App">
         <h5 className='hh'> Country loaded: {coutries.length}</h5>
+        <p>Country added: {Cart.length}</p>
         <ul className='country'>
           {
-            coutries.map(country => <Country country={country}></Country>) 
+            coutries.map(country => <Country country={country} handleAddCountry={handleAddCountry}></Country>) 
           }
         </ul>
     </div>
--- src/Componints/Country/Country.js
+++ src/Componints/Country/Country.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Country.css';
 
 const Country = (props) => {
     const {area, population, flag,} = props.country;
+    const handleAddCountry = props.handleAddCountry;
     return (
-        <div>
+        <div className='CountryStyle'>
             <h5>this is: {area}</h5>
             <p>population: {population}</p>
             <p>flag: {flag}</p>
-            <img src={flag} alt="flag" />
+            <img src={flag} alt="flag"/>
+            <br></br>
+            <button onClick={() =>handleAddCountry(props.country)}>Addd Country</button>
         </div>
     );
 };
